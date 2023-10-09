4261254b9fd9a71a9632a577bffe24ef1f26a25f
ikdg0 <129305617+ikdg0@users.noreply.github.com>
2023-10-09 17:24:01 +0200

    adding popup

---
+++ index.js
@@ -0,0 +1,5 @@
+if (!localStorage.getItem('cookiesAccepted')) {
+    if (confirm('Ce site utilise des cookies. Acceptez-vous les cookies ?')) {
+        localStorage.setItem('cookiesAccepted', true);
+    }
+}
